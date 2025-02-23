33d9e61ce43c75f0b9138c2ce6ea151f06d3b749
harris <marabutharris_bsit@plmun.edu.ph>
2025-02-23 23:43:24 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,14 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        greet();
+        displayMessage();
+    }
+
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+
+    public static void displayMessage() {
+        System.out.println("This is my message!");
+    }
+}
